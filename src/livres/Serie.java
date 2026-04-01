f97de5ba4ef0325602a178f5e0a0105d470f7db3
raphmour <2501848@cegeplimoilou.ca>
2026-04-01 19:51:47 -0400

    DEV1 - Création de la classe Serie

---
+++ src/livres/Serie.java
@@ -0,0 +1,12 @@
+package livres;
+
+import java.util.ArrayList;
+
+public class Serie {
+    public String nom;
+    public ArrayList<Ouvrage> listeOuvrages;
+
+    public Serie(String nom) {
+        this.nom = nom;
+    }
+}
